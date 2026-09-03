5d46a880997a7c86fb018b7b7860580bee026f18
Miles Moore <79930428+milesalanmoore@users.noreply.github.com>
2026-09-02 20:48:11 -0600

    update print_fires for new named column and add run shell script

--- print_fires.py
+++ print_fires.py
@@ -4,5 +4,7 @@ country = "United States of America"
 country_column = 0
 fires_column = 4
 file_name = "data/Agrofood_co2_emission.csv"
-fires = my_utils.get_column(file_name, country_column, country, fires_column)
+fires = my_utils.get_column(
+    file_name, country_column, country, result_column=fires_column
+)
 print(fires)
